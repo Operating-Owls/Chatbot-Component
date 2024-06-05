e351343b71a9c9e39f531d74b7acf11ba803e962
Benjamin Schoolland <bschoolland@gmail.com>
2024-06-05 16:14:28 -0700

    feat: rate limit using a simple dictionary object

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 import os
 from getChatbotMessage import getChatbotMessage
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -15,23 +16,63 @@ print(cors_origins)
 # Apply CORS
 CORS(app, resources={r"/*": {"origins": cors_origins}})
 
-@app.route('/')
-def hello():
-    print('hello world')
-    return jsonify({'message': 'Hello, World'})
+MESSAGE_LIMIT = 2
+RESET_MINUTES=2
+# store sessions
+sessions = {}
 
-@app.route('/api/data')
-def data():
-    return jsonify({'message': 'Hello, World!'})
+def generate_session_id(request):
+    user_agent = request.headers.get('User-Agent')
+    ip_address = request.remote_addr
+    session_id = hash((user_agent, ip_address))
+    return session_id
+
+
+def filter_old_messages(data):
+    # Define the current time
+    current_time = datetime.now()
+    
+    # Define the time limit based on RESET_HOURS
+    reset_time_limit = current_time - timedelta(minutes=RESET_MINUTES)
+    
+    # Filter and keep data that is newer than RESET_HOURS
+    filtered_data = [message for message in data if datetime.strptime(message['time'], '%Y-%m-%d %H:%M:%S') > reset_time_limit]
+    
+    return filtered_data
+
+
+def get_session_data(session_id):
+    if session_id not in sessions:
+        sessions[session_id] = []
+    data = sessions[session_id]
+    data = filter_old_messages(data)
+    return data
+
+def add_message(session_id, text):
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Format the current time as a string
+    message = {'text': text, 'time': current_time}
+    if session_id not in sessions:
+        sessions[session_id] = []  # Initialize the session if it doesn't exist
+    sessions[session_id].append(message)  # Add the message to the session
+
+def set_session_data(session_id, data):
+    sessions[session_id] = data
 
 # Define the chatbot endpoint
 @app.route('/api/chatbot', methods=['POST'])
 def chatbot():
     try:
+        session_id = generate_session_id(request)
+        data = get_session_data(session_id)
+        if len(data) > MESSAGE_LIMIT:
+            set_session_data(session_id, data)
+            return jsonify({'message': "sorry, you've used all allowed messages. Try again tomorrow"})
+        
         # Get the data from the request
         data = request.get_json()
         # the message from the user
         message = data['message']
+        add_message(session_id, message)
         # Create a chat log with the user message
         chat_log = []
         chat_log.append({"role": "user", "content": message})
