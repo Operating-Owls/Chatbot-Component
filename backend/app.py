b9488fdc88e5cce4851d31380afbd7b533bb6d3d
Benjamin Schoolland <bschoolland@gmail.com>
2024-06-04 19:04:55 -0700

    feat: create backend code

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
+from getChatbotMessage import getChatbotMessage
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -11,16 +12,37 @@ app = Flask(__name__)
 cors_origins = os.getenv('CORS_ORIGINS', '*')
 print(cors_origins)
 
-# Apply CORS to your app
+# Apply CORS
 CORS(app, resources={r"/*": {"origins": cors_origins}})
 
 @app.route('/')
 def hello():
+    print('hello world')
     return jsonify({'message': 'Hello, World'})
 
 @app.route('/api/data')
 def data():
     return jsonify({'message': 'Hello, World!'})
 
+# Define the chatbot endpoint
+@app.route('/api/chatbot', methods=['POST'])
+def chatbot():
+    try:
+        # Get the data from the request
+        data = request.get_json()
+        # the message from the user
+        message = data['message']
+        # Create a chat log with the user message
+        chat_log = []
+        chat_log.append({"role": "user", "content": message})
+        # Get the response from the chatbot
+        chatBotResponse = getChatbotMessage(chat_log)
+        # Return the response
+        return jsonify({'message': chatBotResponse})
+    except Exception as e:
+        # Return an error in case of an exception
+        print("Error:", e)
+        return jsonify({'message': 'An unexpected error occurred.'})
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
